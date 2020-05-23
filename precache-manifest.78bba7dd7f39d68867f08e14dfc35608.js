79d87fd3cf7af224a2afa13aa246dc5e93683dd5
ruben <cameroamadorr@gmail.com>
2020-05-23 17:08:38 -0500

    first commit

---
+++ precache-manifest.78bba7dd7f39d68867f08e14dfc35608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8166fbefc717e5a5dcc98e5629f90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "600c084b229bc4aa4e4a",
+    "url": "/static/css/2.6217cd2f.chunk.css"
+  },
+  {
+    "revision": "61e1e9899fe7688657a5",
+    "url": "/static/css/main.4e2121b3.chunk.css"
+  },
+  {
+    "revision": "600c084b229bc4aa4e4a",
+    "url": "/static/js/2.6aa21dc1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6aa21dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e1e9899fe7688657a5",
+    "url": "/static/js/main.c384a742.chunk.js"
+  },
+  {
+    "revision": "c301adf3e3b12f3e779b",
+    "url": "/static/js/runtime-main.35e22bb8.js"
+  }
+]);
